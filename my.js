c3bab79644d99245f8526a7058fde4844b79139d
Fateme MavandadiPour <lfawtme@gmail.com>
2023-10-16 10:43:51 +0330

    function

--- my.js
+++ my.js
@@ -1,4 +1,14 @@
-function totalLength(x, y) {
-    var total = x.length - y.length;
+function fullname(x, y) {
+    return x + " " + y;
+}
+function totallength(x, y) {
+    var total = x.length + y.length;
+    x.slice(0);
+    if (x instanceof Array) {
+    }
+    if (x instanceof String) {
+    }
     return total;
 }
+totallength("fateme", "mavandadi");
+totallength(["ali", "hossein", "reza"], ["alizade", "dehqan", "mavandadi"]);
--- my.ts
+++ my.ts
@@ -1,7 +1,25 @@
 
-
-function totalLength(x: (string | any[]),y:(string | any[])) :number
+function fullname(x:String, y:string, c:string):string
+function fullname(x:string , y:string):string
 {
-    var total: number= x.length - y.length;
+    return x+" "+y;
+}
+
+function totallength(x:string,y:string):number
+function totallength(x:any[],y:any[]):number
+function totallength(x: string | any[],y:string | any[])
+{
+    var total :number =x.length+y.length;
+    x.slice(0)
+    if (x instanceof Array){
+
+    }
+   if (x instanceof String){
+
+   }
+
+
     return total;
 }
+totallength("fateme","mavandadi")
+totallength(["ali","hossein","reza"],["alizade","dehqan","mavandadi"])
