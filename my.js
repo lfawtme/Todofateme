346e73434f1a7ace0bc643756303f6ea4d58b7d7
Fateme MavandadiPour <lfawtme@gmail.com>
2023-10-19 09:37:10 +0330

    enum

--- my.js
+++ my.js
@@ -1,9 +1,37 @@
+var gender;
+(function (gender) {
+    gender[gender["male"] = 1] = "male";
+    gender[gender["female"] = 2] = "female";
+})(gender || (gender = {}));
 function register(person) {
     console.log(person.name + " " + person.family);
 }
 var iman = {
     name: "fateme",
     family: "mavandadi",
-    age: 23
+    age: 23,
+    gender: gender.female,
 };
 register(iman);
+var days;
+(function (days) {
+    days[days["shanbe"] = 0] = "shanbe";
+    days[days["yek"] = 1] = "yek";
+    days[days["doshanbe"] = 2] = "doshanbe";
+    days[days["seshanbe"] = 3] = "seshanbe";
+})(days || (days = {}));
+var colors;
+(function (colors) {
+    colors["red"] = "#0cxxff";
+    colors["green"] = "#3xfccc";
+})(colors || (colors = {}));
+console.log(colors.green);
+function SayHello(type) {
+    if (type == gender.female) {
+        console.log("hello female");
+    }
+    if (type === gender.male) {
+        console.log("hello male");
+    }
+}
+console.log(SayHello(gender.female));
--- my.ts
+++ my.ts
@@ -2,17 +2,23 @@ interface IPerson {
     name:String,
     family: string,
     age: number,
+    gender: gender,
     email?:string
 }
-function register(person:IPerson){
+enum gender{
+   male=1,
+   female=2,
+}
 
+function register(person:IPerson){
  console.log(person.name+" "+person.family)
 
 }
  var iman:IPerson= {
     name : "fateme",
     family: "mavandadi",
-    age: 23
+    age: 23,
+    gender:gender.female,
  }
 
  register(iman);
@@ -22,3 +28,24 @@ function register(person:IPerson){
     getall():IPerson[],
     getById(personId:number):IPerson,
  }
+ enum days{
+   shanbe,
+   yek ,
+   doshanbe,
+   seshanbe,
+ }
+ enum colors {
+   red= "#0cxxff",
+   green="#3xfccc"
+ }
+ console.log(colors.green)
+
+ function SayHello (type: gender){
+ if (type==gender.female){
+   console.log("hello female")
+ }
+if (type===gender.male){
+   console.log("hello male")
+}
+ }
+ console.log(SayHello(gender.female))
